126577bd27d287c2abd900bb9992784e4140c557
Number279 <sawyercarroll@gmail.com>
2021-03-12 14:33:19 -0600

    first commit

---
+++ src/test/java/com/tts/NonProfitApp/NonProfitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.NonProfitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonProfitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
