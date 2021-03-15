568b98b8292f391a7b945ed8100603db9d56a96f
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-03-15 10:45:05 -0500

    Created Product Repository

---
+++ src/main/java/com/tts/NonProfitApp/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.tts.NonProfitApp.repository;
+
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findAll();
+    Product findById(long id);
+    List<Product> findByCategory(String category);
+
+    @Query("SELECT DISTINCT p.category FROM Product p")
+    List<String> findDistinctCategories();
+
+}
