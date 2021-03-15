99e537c8d57e72f458cedd6b40206ba0f034246e
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-03-15 10:48:10 -0500

    Added import

--- src/main/java/com/tts/NonProfitApp/repository/ProductRepository.java
+++ src/main/java/com/tts/NonProfitApp/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.tts.NonProfitApp.repository;
 
-
+import com.tts.NonProfitApp.model.Product;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
