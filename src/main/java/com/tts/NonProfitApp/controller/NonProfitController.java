6ec0b5d2e4d65ead0ee95b2aea6a60342f0586f8
Number279 <sawyercarroll@gmail.com>
2021-03-18 19:50:39 -0500

    Add @CrossOrigin annotation GetMapping

--- src/main/java/com/tts/NonProfitApp/controller/NonProfitController.java
+++ src/main/java/com/tts/NonProfitApp/controller/NonProfitController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @Api(value = "nonProfits", description = "Rest Controller to handle Crud actions on nonProfit database.")
 @RequestMapping("/nonprofits")
@@ -46,6 +47,21 @@ public class NonProfitController {
     @GetMapping("/all")
     public Iterable<User> findAll() { return userService.findAll();}
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved the Non-Profits for this city."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "All non-profits for a given city.", response = User.class,
+            responseContainer = "List")
+    @CrossOrigin
+    @GetMapping("/city")
+    public List<User> sortByCity(@RequestParam(value = "city", required = false)String city){
+        if(city!=null){
+            return userService.sortByCity(city);
+        }
+        return userService.findAll();
+    }
+
 //    @ApiResponses(value = {
 //            @ApiResponse(code=200, message="Successfully retrieved the Non-Profits for this city."),
 //            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
@@ -53,29 +69,15 @@ public class NonProfitController {
 //    @ApiOperation(value = "All non-profits for a given city.", response = User.class,
 //            responseContainer = "List")
 //    @GetMapping("/city")
-//    public List<User> sortByCity(@RequestParam(value = "city", required = false)String city){
+//    public ResponseEntity<List<User>> sortByCity(@RequestParam(value = "city", required = false)String city){
+//        HttpHeaders responseHeaders = new HttpHeaders();
+//        responseHeaders.set("Sawyer's Header", "Access-Control-Allow-Origin: *");
+//
 //        if(city!=null){
-//            return userService.sortByCity(city);
+//            return ResponseEntity.ok(userService.sortByCity(city));
+////                    .headers(responseHeaders)
+////                    .body("Response with header using ResponseEntity");
 //        }
-//        return userService.findAll();
+//        return ResponseEntity.ok(userService.findAll());
 //    }
-
-    @ApiResponses(value = {
-            @ApiResponse(code=200, message="Successfully retrieved the Non-Profits for this city."),
-            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
-    })
-    @ApiOperation(value = "All non-profits for a given city.", response = User.class,
-            responseContainer = "List")
-    @GetMapping("/city")
-    public ResponseEntity<List<User>> sortByCity(@RequestParam(value = "city", required = false)String city){
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Sawyer's Header", "Access-Control-Allow-Origin: *");
-
-        if(city!=null){
-            return ResponseEntity.ok(userService.sortByCity(city));
-//                    .headers(responseHeaders)
-//                    .body("Response with header using ResponseEntity");
-        }
-        return ResponseEntity.ok(userService.findAll());
-    }
 }
